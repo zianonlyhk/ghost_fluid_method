686ca0c874dd3f0b589e258484f952c9fd68ba57
Zian Huang <zianonlyhk@gmail.com>
2023-02-10 14:42:23 +0000

    fixed min/max typo in 1D HLLC

--- src/ghost_fluid_utilities.cc
+++ src/ghost_fluid_utilities.cc
@@ -424,7 +424,7 @@ std::array<double, 3> GhostFluidUtilities::HLLC_1D(std::array<double, 3> i_W_L, 
     }
 
     double S_L = std::min(u_L - a_L * q_L, u_R - a_R * q_R);
-    double S_R = std::min(u_L + a_L * q_L, u_R + a_R * q_R);
+    double S_R = std::max(u_L + a_L * q_L, u_R + a_R * q_R);
 
     double S_star = (p_R - p_L + rho_L * u_L * (S_L - u_L) - rho_R * u_R * (S_R - u_R)) / (rho_L * (S_L - u_L) - rho_R * (S_R - u_R));
 
